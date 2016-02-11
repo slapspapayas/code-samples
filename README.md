a4c52fbb9be07df918159da8e8f94aedb21a51f7
slapspapayas <apappas@adcade.com>
2016-02-11 15:37:59 -0500

    Samples from long ass time ago

---
+++ README.md
@@ -0,0 +1,20 @@
+## Code Samples
+
+* DOM Manipulation
+
+* Templates
+ * Jade sample
+ * Handlebars..?
+
+* CSS/Preprocessors??
+ * Stylus
+ * Less
+ * Sass
+
+* JavaScript
+ * couple things from fw
+ * ummmmmmmmm
+ * Node scripts?? errrr
+
+* React/ES6
+ * file tree
---
+++ dom-manipulation/links.js
@@ -0,0 +1,50 @@
+/*
+ * This script will find all CMS generated links and
+ * if they are to sections of current page add smooth-scroll
+ */
+(function() {
+    window.addEventListener('DOMContentLoaded', function() {
+        var content = document.querySelector('.cms-created');
+        if (!!content) {
+            // We will touch a LOT of DOM content so let's use a fragment
+            var docFragment = document.createDocumentFragment();
+            docFragment.appendChild(content.cloneNode(true));
+            var links = docFragment.querySelectorAll('a');
+
+            // Only care about in page links
+            var currPage = window.location.pathname;
+            var hash = window.location.hash;
+
+            // Decrementing is faster and we have a lot of links
+            for (i = links.length; i--;) {
+                var link = links[i];
+
+                if (link.pathname === currPage && link.hash) {
+                    // Add smooth-scroll
+                    link.setAttribute('data-scroll', '');
+
+                    var target = docFragment.querySelector(link.hash);
+                    var section = target ? target.classList.contains('page-section') : false;
+                    // If link is not to a page section we need a larger offset
+                    if (!section) {
+                        link.setAttribute('data-options', '{"offset":140}');
+                    }
+                }
+            }
+
+            // Replaced live DOM content with fragment
+            content.parentNode.replaceChild(docFragment, content);
+
+            if (hash) {
+                // Make sure we offset for hash on inital load
+                var el = document.querySelector(hash);
+                if (smoothScroll && el) {
+                    var offset = el.classList && el.classList.contains('page-section') ? 95 : 140;
+                    smoothScroll.animateScroll(el, hash, {
+                        offset: offset
+                    });
+                }
+            }
+        }
+    }, false);
+})();
